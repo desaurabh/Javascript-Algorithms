4cf5bc9c4be092a815c72efc950b80d334d9e5fc
Saurabh kumar <desaurabh@live.com>
2015-04-24 01:46:39 +0530

    Algorithm 1 :refractored code

--- CalculateSets.js
+++ CalculateSets.js
@@ -1,9 +1,8 @@
-﻿var setSize=function(dataLength, expectedSetSize){
-	var set=dataLength/expectedSetSize;
-	var rem=dataLength%expectedSetSize; 
-	var result; 
-	if(rem!=0){
+﻿var set;
+var calculateSetSize=function(dataLength, expectedSetSize){
+	set=dataLength/expectedSetSize;
+	if((dataLength%expectedSetSize)!=0){
 		var strSet=set.toString().split(".")[0]; 
 		var numSet=parseInt(strSet[0]); 
-		result=numSet+1;} 
-	return result;}
+		set=numSet+1;} 
+	return set;}
