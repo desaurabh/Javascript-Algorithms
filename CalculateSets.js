b74f8f89b4d6297b0f637f50621c95ba13c2df4c
Saurabh kumar <desaurabh@live.com>
2015-04-24 01:54:09 +0530

    tweak a bit

--- CalculateSets.js
+++ CalculateSets.js
@@ -3,6 +3,6 @@ var calculateSetSize=function(dataLength, expectedSetSize){
 	set=dataLength/expectedSetSize;
 	if((dataLength%expectedSetSize)!=0){
 		var strSet=set.toString().split(".")[0]; 
-		var numSet=parseInt(strSet[0]); 
+		var numSet=parseInt(strSet); 
 		set=numSet+1;} 
 	return set;}
