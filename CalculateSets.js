ed467eba9873179541f4cea535feb51cbaf07b54
Saurabh kumar <desaurabh@live.com>
2015-04-23 23:18:17 +0530

    Algorith 1: refractored a bit works fine

--- CalculateSets.js
+++ CalculateSets.js
@@ -1,1 +1,9 @@
-﻿var sets=function(dataLength, expectedSetSize){var set=dataLength/expectedSetSize; var rem=dataLength%expectedSetSize; var result; if(rem!=0){var setStr=set.toString(); var setNum=parseInt(setStr[0]); result=setNum+1;} return result;}
+﻿var setSize=function(dataLength, expectedSetSize){
+	var set=dataLength/expectedSetSize;
+	var rem=dataLength%expectedSetSize; 
+	var result; 
+	if(rem!=0){
+		var strSet=set.toString().split(".")[0]; 
+		var numSet=parseInt(strSet[0]); 
+		result=numSet+1;} 
+	return result;}
